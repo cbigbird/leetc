f841d96403496e0c5b2aa8fdc0b8c573a89874e2
Perry Wang <perryw055@gmail.com>
2022-11-22 19:51:43 +0800

    empty p26

---
+++ src/P26RemoveDuplicates.java
@@ -0,0 +1,16 @@
+public class P26RemoveDuplicates {
+    public static void main(String[] args) {
+        P26RemoveDuplicates entity = new P26RemoveDuplicates();
+        int[] arr1 = new int[]{};
+        int[] arr2 = new int[]{1};
+        int[] arr3 = new int[]{1,2,3,4,5,6,7,8,9};
+        System.out.println();
+        System.out.println(entity.removeDuplicates(arr1));
+        System.out.println(entity.removeDuplicates(arr2));
+        System.out.println(entity.removeDuplicates(arr3));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        return 0;
+    }
+}
